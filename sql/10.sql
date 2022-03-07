c3be988fc0742b29c689bdd3f4627299c71a5adb
Julie <78115182+leafsphere@users.noreply.github.com>
2022-03-06 23:55:43 -0800

    simplified code

--- sql/10.sql
+++ sql/10.sql
@@ -3,9 +3,8 @@
  * Use tables film and film_actor.
  */
 
-SELECT title, film.film_id, 
-COUNT(actor_id) AS actor_count 
-FROM film LEFT JOIN film_actor ON film.film_id = film_actor.film_id 
-GROUP BY film.film_id 
-HAVING COUNT(actor_id) >= 1 
+SELECT title, film.film_id, COUNT(actor_id) AS actor_count 
+FROM film
+JOIN film_actor ON (film.film_id = film_actor.film_id)
+GROUP BY film.film_id
 ORDER BY actor_count, title;
