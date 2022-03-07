e985d1bde894453d899a5601393efaa898fa442f
Julie <78115182+leafsphere@users.noreply.github.com>
2022-03-06 23:59:26 -0800

    formatting

--- sql/12.sql
+++ sql/12.sql
@@ -6,6 +6,6 @@
 
 SELECT customer.customer_id, first_name, last_name, SUM(amount) as sum
 FROM customer
-LEFT JOIN payment ON payment.customer_id = customer.customer_id
+JOIN payment ON (payment.customer_id = customer.customer_id)
 GROUP BY customer.customer_id
 ORDER BY last_name;
