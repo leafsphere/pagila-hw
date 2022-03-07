60dc3ba798deda9f6807474078709a33138179b1
Julie <78115182+leafsphere@users.noreply.github.com>
2022-03-07 00:04:55 -0800

    formatting

--- sql/14.sql
+++ sql/14.sql
@@ -5,11 +5,8 @@
 
 SELECT title
 FROM film
-WHERE film_id IN (
-    SELECT film_id
-    FROM film_category
-    LEFT JOIN category ON film_category.category_id = category.category_id
-    WHERE name = 'Family'
-)
+JOIN film_category ON (film_category.film_id=film.film_id)
+JOIN category USING (category_id)
+WHERE name='Family'
 ORDER BY title;
 
