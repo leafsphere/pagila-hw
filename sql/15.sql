12da8d5378b6ee47e15c0850e111afa8073ac656
Julie <78115182+leafsphere@users.noreply.github.com>
2022-03-07 00:10:39 -0800

    simplified code

--- sql/15.sql
+++ sql/15.sql
@@ -3,19 +3,30 @@
  * Use table category, film_category, film, and language.
  */
 
+SELECT category.name, count(*) AS sum
+FROM category
+JOIN film_category USING (category_id)
+JOIN film USING (film_id)
+JOIN language USING (language_id)
+WHERE language.name='English'
+GROUP BY category.name
+ORDER BY category.name;
+
+/*
 SELECT name, count(*) AS sum
 FROM film
 LEFT JOIN (
 	SELECT film_id, name
 	FROM film_category
-	LEFT JOIN category ON film_category.category_id = category.category_id
+	LEFT JOIN category ON (film_category.category_id = category.category_id)
 ) film_id_category
-ON film.film_id = film_id_category.film_id
+ON (film.film_id = film_id_category.film_id)
 WHERE language_id IN (
 	SELECT film.language_id
 	FROM language
-	RIGHT JOIN film ON film.language_id = language.language_id
+	RIGHT JOIN film ON (film.language_id = language.language_id)
 	WHERE name = 'English'
 )
 GROUP BY name
 ORDER BY name;
+*/
